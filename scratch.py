52161422ab0c8cb0a48ac7fae05c2faf28856ab7
mwamw <13081969068@163.com>
2026-04-22 23:24:07 +0800

    chore: cleanup repository, fix .gitignore and remove cached artifacts

--- scratch.py
+++
@@ -1,3 +0,0 @@
-from google import genai
-client = genai.Client(api_key="123", http_options={'base_url': 'http://foo'})
-print("Success! base_url:", client._api_client.base_url if hasattr(client, '_api_client') else client)
--- test_openai.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-import pydantic
-from openai.types.chat.chat_completion_message import ChatCompletionMessage
-
-print("ChatCompletionMessage fields:")
-pprint(list(ChatCompletionMessage.model_fields.keys()))
-
